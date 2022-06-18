ea1d1b64d0e95fac418c8e2bedc6903523dae475
VictorCosta7 <engvictorcosta10@gmail.com>
2022-06-18 12:50:42 -0300

    adição do ContactComponent

--- client/src/components/ContactComponent.vue
+++ client/src/components/ContactComponent.vue
@@ -1,7 +1,83 @@
 <template>
-  <div>
-    <p>Contact</p>
-  </div>
+  <section id="contact">
+    <div id="titleContact">
+      <span>{{ number }}</span>
+      <h1>{{ title }}</h1>
+    </div>
+    <p id="description">{{ description }}</p>
+    <div id="links">
+      <a href="https://github.com/VictorCosta7">
+        <b-icon id="link" icon="github"></b-icon>
+      </a>
+      <a href="https://www.linkedin.com/in/victor-costa-92a9441b7/">
+        <b-icon id="link" icon="linkedin"></b-icon>
+      </a>
+      <a href="https://twitter.com/Victor5Costa">
+        <b-icon id="link" icon="twitter"></b-icon>
+      </a>
+      <a href="https://www.instagram.com/vrodrigo.costa/">
+        <b-icon id="link" icon="instagram"></b-icon>
+      </a>
+    </div>
+  </section>
 </template>
 
-<style scoped></style>
+<script>
+export default {
+  data() {
+    return {
+      number: "04.",
+      title: "Algum texto",
+      description:
+        "Atualmente estou a procura de oportunidades de emprego, Estou sempre aberto para contato e tento responder o mias rapido possivel bla bla bla...",
+    };
+  },
+};
+</script>
+
+<style scoped>
+#contact {
+  background-color: var(--dark-blue);
+}
+
+#titleContact {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+#description {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 4rem;
+}
+
+#titleContact span {
+  font-size: 2.4rem;
+  padding-right: 0.6rem;
+  color: var(--light-green);
+}
+
+#links {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  font-size: 3rem;
+}
+
+#link {
+  color: var(--white-soft);
+}
+
+@media (max-width: 768px) {
+  #contact {
+    width: 100%;
+    padding: 2rem;
+  }
+}
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,10 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from "vue";
+import App from "./App.vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 new Vue({
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
