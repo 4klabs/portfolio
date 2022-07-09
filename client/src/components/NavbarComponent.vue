f0567cec0d63919ff1a690859b36c45810360f8f
Matheus Nascimento <103283347+matheuscont@users.noreply.github.com>
2022-07-09 17:32:57 -0300

    Code matheus (#18)
    * Criação do Navbar e primeiros detalhes do front.
    
    * NavBar atualizado

--- client/src/components/NavbarComponent.vue
+++ client/src/components/NavbarComponent.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="header">
-    <img src="">
-    <a> <h2> {{ about }}</h2></a>
-    <a><h2> {{ experience }} </h2></a>
-    <a><h2> {{ work }} </h2></a>
-    <a><h2> {{ contact }} </h2></a>
-    <a href="#CV"><h2> {{ resumo }} </h2></a>
+
+    <a id="logo"> 
+      <img id="brand" src="../assets/logo-mn.png">
+    </a>
+    <button id="btn-mobile">MENU</button>
+    <a class="navbarItem1"><h2> {{ about }} </h2></a>
+    <a class="navbarItem2"><h2> {{ experience }} </h2></a>
+    <a class="navbarItem3"><h2> {{ work }} </h2></a>
+    <a class="navbarItem4"><h2> {{ contact }} </h2></a>
+    <a class="navbarItem5" href="#CV"><button id="link-cv">{{ resume }}</button></a>
   </div>
 </template>
 
 <script>
-  export default {
+
+  export default {   
     data () {
       return {
         about: '01. Sobre Mim',
@@ -24,15 +29,48 @@
 </script>
 
 <style scoped>
-  #header {
+
+ #header {
     background-color: #0a192f;
     font-family: 'Fira Code', monospace;
-    font-size: 13px;
-    color: #e6f1ff;
+    font-size: 11px;
+    color: #e7ffe6;
     display: flex;
     justify-content: flex-end;
-    gap: 70px;
+    gap: 30px;
     align-items: center;
-    padding: 20px 40px;
+    padding: 20px;
+}
+
+#logo {
+  margin: auto;
+  margin-left: 0;
+}
+
+#brand {
+  width: 90px;
+  height: 90px;
+  background-color: #0a192f;
+}
+
+#btn-mobile {
+  display: none;
+}
+
+#link-cv {
+  width:120px;
+  height:50px;
+  font-family: 'Fira Code', monospace;
+  Cursor: pointer;
+  border: 2px solid;
+  color: #64ffda;
+  border-radius: 3px;
+  background-color: #0a192f;
+  border-color: #64ffda;
+  margin-right: 75px;
+}
+
+a:hover #link-cv{
+  background-color: #11294d;
 }
 </style>
