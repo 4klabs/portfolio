4b279e795956dee4ce0e7173f9dc2ac630e32a92
Matheus Nascimento <matheuscont2109@gmail.com>
2022-07-23 16:02:17 -0300

    Changes

--- client/src/components/FooterComponent.vue
+++ client/src/components/FooterComponent.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id= "footer">
-    <p> Copyright © <span> {{ currentYear }}</span></p>
-    <p> Desenvolvido por Matheus Nascimento e Victor Costa </p>
-    <p> All Rights Reserved </p>
+  <div>
+    <p>Copyright © {{ currentYear }}</p>
+    <p>Desenvolvido por Matheus Nascimento e Victor Costa</p>
+    <p>All Rights Reserved</p>
   </div>
 </template>
 
@@ -10,25 +10,24 @@
 export default {
   data() {
     return {
-      currentYear: "2022",
+      currentYear:"2022",
     };
   },
 };
 </script>
 
 <style scoped>
+  div {
+    background-color:#0a192f;
+    text-align:center;
+    font-size:small;
+    color:#A8B2D1;
+  } 
 
-#footer {
-  background-color: #0a192f;
-  text-align: center;
-  font-size: small;
-  color: #A8B2D1;
-}
-
-  @media (max-width: 768px) {
+  @media (max-width:768px) {
   #contact {
-    width: 100%;
-    padding: 2rem;
+    width:100%;
+    padding:2rem;
     }
   }
   
